e097f31ba9d0d0ae4839fb778e1d00c8430f71ac
GraydonE <124379361+GraydonE@users.noreply.github.com>
2023-04-27 14:07:17 +0000

    IT WORKS I DID IT FINALLY RAHHHHHH

--- results.php
+++ results.php
@@ -4,20 +4,28 @@
 $age = $_POST["age"];
 $day = $_POST["day"];
 
-if ($age < 5 || $age > 95) {
-  echo "You can get in for free!";
-}
-  
-else if (($age >= 12 && $age <= 21) || ($day == "tuesday") || ($day == "thursday")) {
-  echo "You get a discounted price!";
+
+//Display if user can watch r rated movies
+if ($age <= 5 && $age >= 0 || $age >= 95 && $age <= 120) {
+  echo "You can enter for free.";
+
 }
 
-else if (($age > 21) || ($age >=5)) {
-  echo  "You pay the regular price";   
+  // Display if user can watch G rated movies
+else if ($age >= 12 && $age <= 21 || $day== "tuesday" || $day == "thursday") {
+  echo "You get the student discount.";
+
+}
+
+  // Display if user has to pay full price
+else if ($age >= 0 && $age <=120) {
+  echo "You have to pay full price.";
+
 }
   
-else ($age == 0) {
-  echo "Enter a valid age";  
+// Display is the user does not enter a valid age
+else {
+  echo "Enter a valid age.";    
 }
 
 ?>
